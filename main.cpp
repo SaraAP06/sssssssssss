f392e01c1afbcba5a4ed82e1ddcb447a51753998
SaraAP06 <sara.ap4438@gmail.com>
2025-07-01 18:52:02 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,9 @@
 #include <QCoreApplication>
 #include "server.h"
 #include "database.h"
-#include <QFile>
-#include <QTextStream>
-#include <QDateTime>
-
-// ثبت لاگ
-void logMessage(const QString& message) {
-    QFile logFile("server.log");
-    if (logFile.open(QIODevice::Append | QIODevice::Text)) {
-        QTextStream out(&logFile);
-        out << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss") << ": " << message << "\n";
-        logFile.close();
-    }
-}
 
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
-
-    // ثبت لاگ شروع برنامه
-    logMessage("برنامه سرور شروع شد");
 
     // راه‌اندازی و تنظیم جداول دیتابیس
     Database::instance().setupTables();
@@ -27,10 +11,11 @@ int main(int argc, char *argv[]) {
     // راه‌اندازی سرور
     Server server;
     if (!server.startServer(12345)) {
-        qCritical() << "Failed to start server";
-        logMessage("خطا در راه‌اندازی سرور");
+        qCritical() << "خطا در راه‌اندازی سرور";
+        server.logMessage("خطا در راه‌اندازی سرور");
         return 1;
     }
 
+    server.logMessage("برنامه سرور شروع شد");
     return a.exec();
 }
